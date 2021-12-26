91d8a09dcc363604e5e0ff5af329e55bf7d9b71f
Phyya <80838239+Phyya@users.noreply.github.com>
2021-12-26 20:28:37 +0100

    Updated

--- src/components/NewChat.jsx
+++ src/components/NewChat.jsx
@@ -85,7 +85,7 @@ const NewChat = ({ friends, user }) => {
 
         <div className="display__chat">
 {friends.length}
-          {friends.length > 0 ? (
+          {friendslist.length > 0 ? (
             <div className="contact__list">
               {contacts.length === 0 ? (
                 friendsSorted.map((contact, index) => {
