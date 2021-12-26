0f060181d05843085b3bed258f3015543fbe06f0
Phyya <80838239+Phyya@users.noreply.github.com>
2021-12-26 20:12:39 +0100

    Updated

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -19,6 +19,8 @@ setFriendsList(filtered)
   return (
     <div className="sidebar">
       <div className="sidebar__chats">
+{friendsList.map(f=> (<p> {f.name}</p>))
+}
         <NewChat user={user} friends={friendsList} />
       </div>
     </div>
