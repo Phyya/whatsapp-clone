0ed5825ea4d9914e6dff70e5e889ac05d15d2f80
Phyya <80838239+Phyya@users.noreply.github.com>
2021-12-26 23:33:12 +0100

    Updated

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,4 @@
-import {useEffect, useState} from "react"
+import {useState} from "react"
 import NewChat from "./NewChat";
 import "../cssFolder/Sidebar.css";
 
@@ -6,21 +6,13 @@ import "../cssFolder/Sidebar.css";
 
 const Sidebar = ({ user }) => {
 
-const [friendsList, setFriendsList] = useState([])
-
-const database =JSON.parse(localStorage.getItem("database"))
-useEffect(()=> {
-const filtered = database.filter((user) => user.messages.length !== 0)
-setFriendsList(filtered)
-}
-)
 
 
   return (
     <div className="sidebar">
       <div className="sidebar__chats">
-{friendsList.map(f => <p> {f.name} </p>)}
-        <NewChat user={user} friends={friendsList} setFriendsList ={setFriendsList} />
+
+        <NewChat user={user}  />
       </div>
     </div>
   );
